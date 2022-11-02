0633e5ea10a32e3975cb980bdc788a13fd3c2e1d
Vladislav Illarionov <vladislavillarionov32@gmail.com>
2022-11-02 22:48:40 +0300

    rbtree.h -> todo

--- rbtree/src/rbtree/rbtree.h
+++ rbtree/src/rbtree/rbtree.h
@@ -1,1 +1,45 @@
 #pragma once
+
+enum class Color {
+  RED,
+  BLACK,
+};
+
+struct NodeData {
+  int value;
+  int key;
+
+  bool operator<(const NodeData& other) const;
+};
+
+class RBNode {
+ private:
+  Color col;
+  NodeData data;
+  RBNode* left;
+  RBNode* right;
+  RBNode* parent;
+
+ public:
+  RBNode(RBNode* _parent, NodeData data, Color _col = Color::BLACK);
+  bool is_leaf() const;
+  Color color() const;
+  void change_color();  // need???
+  int height() const;
+  int black_height() const;
+
+  friend class RBTree;
+};
+
+class RBTree {
+ private:
+  RBNode* head;
+  void rotate_right(RBNode* target_vertex);
+  void rotate_left(RBNode* target_vertex);
+  void insert_fix(RBNode* target_vertex);
+  void erase_fix(RBNode* target_vertex);
+
+ public:
+  void insert(NodeData data);
+  void erase(NodeData data);
+};
